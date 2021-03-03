1ea68afdbf033329d421ee935f03a8bfde69d6ae
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 07:06:43 +0530

    Add project files.

---
+++ ReadFileFromNetwork/Helpers/Utility.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Management;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace ReadFileFromNetwork.Helpers
+{
+    /// <summary>
+    /// Provide helper functions.
+    /// </summary>
+    class Utility
+    {
+        [DllImport("shlwapi.dll")]
+        public static extern bool PathIsNetworkPath(string pszPath);
+
+        /// <summary>
+        /// Read shared file from UNC path.
+        /// </summary>
+        /// <param name="networkName">The network name as \\192.168.1.202\data</param>
+        /// <param name="networkFile">The network file as \\192.168.1.202\data\AboutMe.txt</param>
+        /// <returns>Return value from file</returns>
+        public static string ReadShareFile(string networkName, string networkFile)
+        {
+            string fileText;
+            if (PathIsNetworkPath(networkFile))
+            {
+                var nw = GetNetworkCredential();
+                using (new NetworkConnection(networkName, nw))
+                {
+                    // Read file
+                    if (File.Exists(networkFile))
+                    {
+                        fileText = File.ReadAllText(networkFile);
+                    }
+                    else
+                    {
+                        throw new Exception(string.Format("File '{0}' not found.", networkFile));
+                    }
+                }
+            }
+            else
+            {
+                // Read file
+                if (File.Exists(networkFile))
+                {
+                    fileText = File.ReadAllText(networkFile);
+                }
+                else
+                {
+                    throw new Exception(string.Format("File '{0}' not found.", networkFile));
+                }
+            }
+
+            return fileText;
+        }
+
+        /// <summary>
+        /// Get UNC path from the path passed
+        /// </summary>
+        /// <param name="filePath">The file path that need to be evaluated</param>
+        /// <returns>Return UNC path</returns>
+        public static string GetUNCPath(string filePath)
+        {
+            var path = filePath.TrimEnd('\\', '/') + Path.DirectorySeparatorChar;
+            var dirInfo = new DirectoryInfo(path);
+            var root = dirInfo.Root.FullName.TrimEnd('\\');
+
+            if (!root.StartsWith(@"\\"))
+            {
+                var mo = new ManagementObject
+                {
+                    Path = new ManagementPath(string.Format("Win32_LogicalDisk='{0}'", root))
+                };
+
+                // Get drive type and set path as per that
+                var driveType = Convert.ToUInt32(mo["DriveType"]);
+
+                switch (driveType)
+                {
+                    case 0: // Unknown
+                    case 1: // No root directory
+                    case 2: // Removable disk
+                    case 5: // Compact Disk
+                    case 6: // RAM Disk
+                        break;
+
+                    case 3: // Local disk
+                         // root = string.Format(@"\\{0}\{1}$\", Dns.GetHostName(), root.TrimEnd(':'));
+                        break;
+
+                    case 4: // Network Drive
+                        root = Convert.ToString(mo["ProviderName"]);
+                        filePath = Recombine(root, dirInfo);
+                        break;
+                }
+            }
+
+            return filePath;
+        }
+
+        #region Helpers
+
+        private static string Recombine(string root, DirectoryInfo dirInfo)
+        {
+            var stack = new Stack();
+            while (dirInfo.Parent != null)
+            {
+                stack.Push(dirInfo.Name);
+                dirInfo = dirInfo.Parent;
+            }
+
+            while (stack.Count > 0)
+            {
+                root = Path.Combine(root, (string)stack.Pop());
+            }
+            return root;
+        }
+
+        /// <summary>
+        /// Get network credential object.
+        /// </summary>
+        private static NetworkCredential GetNetworkCredential()
+        {
+            var nw = new NetworkCredential
+            {
+                Domain = "domain",
+                UserName = "Administrator",
+                Password = "Pass99"
+            };
+
+            return nw;
+        }
+
+        #endregion
+
+    }
+}
---
+++ ReadFileFromNetwork/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using ReadFileFromNetwork.Helpers;
+
+namespace ReadFileFromNetwork
+{
+    class Program
+    {
+        // 'data' is the share name and also folder on resepctive system
+        const string NetworkName = @"\\192.168.1.202\data";
+        const string NetworkFile = @"\\192.168.1.202\data\AboutMe.txt";
+
+        // forward slash / used in web addresses 
+        // backward slash \ used in UNC paths
+
+        private static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Reading file from network share or mapped drive...\n\n");
+
+                ReadFromUncPath();
+
+                // var text = File.ReadAllText(@"Z:\AboutMe.txt");
+                // Console.WriteLine(text);
+
+                //GetUncPath();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ResetColor();
+            }
+
+            // Hold till we read
+            Console.ReadLine();
+        }
+
+        private static void ReadFromUncPath()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            var text = Utility.ReadShareFile(NetworkName, NetworkFile);
+            Console.WriteLine("Text from shared folder -\n\n{0}", text);
+            Console.ResetColor();
+        }
+
+        private static void GetUncPath()
+        {
+            //const string path = @"Z:\25UsersImport.csv";
+            const string path = @"C:\25UsersImport.csv";
+
+            if (path.StartsWith(@"\") && Utility.PathIsNetworkPath(path))
+            {
+                //ReadFromUncPath();
+                Console.ForegroundColor = ConsoleColor.Green;
+                var uncPath = Utility.GetUNCPath(path);
+                Console.WriteLine("The UNC path returned for path '{0}' -\n\n{1}", path, uncPath);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                var uncPath = Utility.GetUNCPath(path);
+                Console.WriteLine("The UNC path returned for path '{0}' -\n\n{1}", path, uncPath);
+                Console.ResetColor();
+            }
+
+        }
+    }
+}
